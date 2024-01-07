d79e0517b35484c43a7cac46e8d5b798cee0dc29
Ichbinositu <hesbon.ezekiel@student.moringaschool.com>
2024-01-07 08:32:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (activity='roller-skate') {
+    console.log(`This Saturday, I want to ${activity}`);
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork (activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork(activity);
+
+
+function wrapAdjective(star="*"){
+    {
+        return function (moon = "special") {
+            return `You are ${star}${moon}${star}!`
+        }
+    }
+}
